ec436b2f4aa0f29e6186e4a69da31fdde7098c60
Apple1501 <kunure15@gmail.com>
2022-05-14 14:05:20 +0300

    Did all Tests

--- tests/functional_tests/test_index.py
+++ tests/functional_tests/test_index.py
@@ -4,7 +4,7 @@ import os
 
 from selenium.webdriver.common.by import By
 
-chrome_driver = "C:/Users/IIoT_Lab/PycharmProjects/flaskProject/tests/functional_tests/yandexdriver.exe" if "ChromeWebDriver" not in os.environ.keys() else os.path.join(
+chrome_driver = "D:/0/Доки/2 сем/Программное обеспечение облачных платформ/PipelinePractice/tests/functional_tests/chromedriver.exe" if "ChromeWebDriver" not in os.environ.keys() else os.path.join(
     os.environ["ChromeWebDriver"], 'chromedriver.exe')
 
 
